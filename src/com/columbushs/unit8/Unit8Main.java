6e76b17b2925e5660f04a632f0f5ba4c8276b002
O-Siman <osiman23376@columbushs.com>
2022-01-07 11:16:12 -0500

    Forgot these

---
+++ src/com/columbushs/unit8/Unit8Main.java
@@ -0,0 +1,8 @@
+package com.columbushs.unit8;
+
+public class Unit8Main {
+    public static void main(String[] args) {
+        Unit8_1Arrays2D twoDArrays = new Unit8_1Arrays2D();
+        twoDArrays.TwoDArrayMethods();
+    }
+}
